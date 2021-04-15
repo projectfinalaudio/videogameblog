da2c94a568dee79bc357d0588a33addf1489025b
aig <aig@localhost.localdomain>
2021-04-15 23:47:53 +0300

    Test code

---
+++ app.js
@@ -0,0 +1,33 @@
+/*
+function prac() {
+    var myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for (i=0;i<myArray.length;i++) {
+        if (i % 2 == 0) {
+            myArray.splice()
+            console.log(myArray[i] + " is an ODD number!");
+        }
+    };
+};
+
+
+console.log(prac());
+*/
+
+/*
+const tips_primes = num => {
+    let arr = Array.from({
+            length: num - 1
+        }).map((x, i) => i + 2),
+        sqroot = Math.floor(Math.sqrt(num)),
+        numsTillSqroot = Array.from({
+            length: sqroot - 1
+        }).map((x, i) => i + 2);
+    numsTillSqroot.forEach(x => (arr = arr.filter(y => y % x !== 0 || y === x)));
+    return arr;
+};
+
+console.log(tips_primes(15));
+
+*/
+
+
